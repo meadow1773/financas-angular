68c6ace473398da058fa129758558e1e9c28b2ac
Victor Hugo <victorhugo.oliveira16@gmail.com>
2026-04-29 13:44:33 -0300

    Enable SSL for database connection

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -9,5 +9,5 @@ export const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: Number(process.env.DB_PORT),
-    // ssl: true,
+    ssl: true,
 })
